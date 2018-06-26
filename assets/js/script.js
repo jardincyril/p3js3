1af217c386fba938f81a8d43377bfaf8561681ec
cyril jardin <cyilroc60@hotmail.fr>
2018-06-26 16:03:51 +0200

    p3js3

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function changecolorgreen(){
+  document.getElementById('text').style.color = 'green';
+}
+function changecolorred() {
+  document.getElementById('text').style.color = 'red';
+}
+function changecolorblue() {
+  document.getElementById('text').style.color = 'blue';
+}
